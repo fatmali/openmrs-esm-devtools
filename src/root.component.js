3a5dbc58dc7329fc6e28c4d4e24b8de45cef612c
Fatma Ali <tumz92@gmail.com>
2019-05-23 13:59:14 +0300

    Changed message

--- src/root.component.js
+++ src/root.component.js
@@ -5,7 +5,7 @@ export default class Root extends React.Component {
     catastrophicError: false
   }
   render() {
-    return this.state.catastrophicError ? null : <div>Hi</div>
+    return this.state.catastrophicError ? null : <div>Hello from Openmrs Module SPA!</div>
   }
   componentDidCatch() {
     this.setState({catastrophicError: true})
